346296f517c39fc2a7cb78d08fa37e348bd68f1e
Jozsef Geng <jozsef.geng@fondofbags.com>
2018-09-11 09:54:28 +0200

    change Namespace for Export Plugin

---
+++ src/FondOfSpryker/Zed/OmsOctopusConnector/Communication/Plugin/Oms/Command/ExportToOctopus.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FondOfSpryker\Zed\OmsOctopusConnector\Communication\Plugin\Oms\Command;
+
+use Orm\Zed\Sales\Persistence\SpySalesOrder;
+use Spryker\Zed\Oms\Business\Util\ReadOnlyArrayObject;
+use Spryker\Zed\Oms\Communication\Plugin\Oms\Command\AbstractCommand;
+use Spryker\Zed\Oms\Dependency\Plugin\Command\CommandByOrderInterface;
+
+/**
+ * @method \FondOfSpryker\Zed\OmsOctopusConnector\Business\OmsOctopusConnectorFacadeInterface getFacade()
+ */
+class ExportToOctopus extends AbstractCommand implements CommandByOrderInterface
+{
+    /**
+     * @param \Orm\Zed\Sales\Persistence\SpySalesOrderItem[] $orderItems
+     * @param \Orm\Zed\Sales\Persistence\SpySalesOrder $orderEntity
+     * @param \Spryker\Zed\Oms\Business\Util\ReadOnlyArrayObject $data
+     *
+     * @return array
+     */
+    public function run(array $orderItems, SpySalesOrder $orderEntity, ReadOnlyArrayObject $data)
+    {
+        $this->getFacade()->exportOrder($orderEntity, $orderItems);
+
+        return [];
+    }
+}
